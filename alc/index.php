7454b49176e4a9364018d051b5f4b2caf000ef2b
guruskid <guruskid@yahoo.com>
2018-04-11 09:49:31 +0100

    First commit Message

---
+++ alc/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bright</title>
+		<meta charset="utf-8">
+		<meta http-equiv="refresh" content="5">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/main.js"></script>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+			<h2 class="logo"><a href="./">HNG Internship 4</a></h2>
+			<nav>
+				<ul>
+					<li><a href="./">HOME</a></li>
+					<li><a href="#">SERVICES</a></li>
+					<li><a href="#">ABOUT US</a></li>
+					<li><a href="#">CONTACT US</a></li>
+				</ul>
+			</nav>
+			</div>
+		</header>
+		<div class="container time">
+			<?php
+    		$currentDateTime = date('Y-m-d H:i:s');
+    		echo $currentDateTime;
+			?>
+		</div>
+		<section>
+			<div class="slider container"></div>
+			<div class="container">
+			<div class="col">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="col">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="col">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			</div>
+			<div class="clear"></div>
+		</section>
+		<footer>
+			<div class="container">
+				<p>&copy; 2018 HNG INTERNSHIP 4</p>
+			</div>
+		</footer>
+	</body>
+</html>
